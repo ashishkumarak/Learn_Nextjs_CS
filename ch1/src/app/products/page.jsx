7144789203abf7c3f2cb5f25af3628b0cea2eb6a
ashishkumarak <121762140+ashishkumarak@users.noreply.github.com>
2025-09-21 12:08:07 +0000

    routing

---
+++ ch1/src/app/products/page.jsx
@@ -0,0 +1,17 @@
+
+
+const Products = () => {
+  return (
+    <>
+
+      <h1> Products page
+      </h1>
+      <hr />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore repellendus nisi laborum modi consequatur. Dolore quasi dignissimos vitae voluptatum quisquam tempora repudiandae aperiam laudantium, in eaque esse repellendus veritatis enim.</p>
+
+
+    </>
+  )
+}
+
+export default Products
