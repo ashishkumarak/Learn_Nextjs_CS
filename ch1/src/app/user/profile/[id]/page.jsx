884650a27e205cea09c7d1f8c1d48074c2b54128
ashishkumarak <121762140+ashishkumarak@users.noreply.github.com>
2025-09-21 12:21:25 +0000

    dynamic route

---
+++ ch1/src/app/user/profile/[id]/page.jsx
@@ -0,0 +1,13 @@
+
+
+const MyProfile = ({ params }) => {
+
+  // console.log(params);
+
+  return (
+    <div>
+      Profile page is dynamic now:- { params.id}</div>
+  )
+}
+
+export default MyProfile
